2d3d9e2c0800cbb327e2b9073fa56b8284b18b8c
bolanebyla <alexleskov2000@gmail.com>
2020-09-24 11:15:35 +0800

    взаимодействие с базой для напоминалки

---
+++ notification_service/storage.py
@@ -0,0 +1,39 @@
+import os
+from pymongo import MongoClient
+
+MONGO_DB_ADDR = os.environ.get('MONGO_DB_ADDR')
+MONGO_DB_PORT = os.environ.get('MONGO_DB_PORT')
+MONGO_DB_DATABASE = os.environ.get('MONGO_DB_DATABASE')
+
+
+class MongodbService(object):
+    _instance = None
+    _client = None
+    _db = None
+
+    @classmethod
+    def get_instance(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls.__init__(cls._instance, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        self._client = MongoClient(f'mongodb://{MONGO_DB_ADDR}:{MONGO_DB_PORT}')
+        self._db = self._client[MONGO_DB_DATABASE]
+
+    def get_data(self, collection) -> list:
+        """возвращает список документов из указанной коллекции"""
+        return list(self._db[collection].find())
+
+    def save_data(self, collection, data: dict):
+        """сохраняет документ в указанную коллекцию"""
+        return self._db[collection].insert_one(data)
+
+    def get_user(self, chat_id: int):
+        return self._db.users.find_one(filter={'chat_id': chat_id})
+
+    def get_schedule(self, group):
+        """возвращает расписание группы"""
+        return self._db.schedule.find_one(filter={'group': group})
+
