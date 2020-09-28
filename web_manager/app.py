fa0b3d87c530a635ea7d4e6d90ed968a4cd3df09
bolanebyla <alexleskov2000@gmail.com>
2020-09-28 16:34:51 +0800

    Сделал выбор групп из бд. Добавил дефолтные значения. Сделал поле chat_id неизменяемым

--- web_manager/app.py
+++ web_manager/app.py
@@ -1,17 +1,15 @@
 import pymongo
-from bson.objectid import ObjectId
 
 from flask import Flask
 import flask_admin as admin
 
-from wtforms import form, fields
+from wtforms import form, fields, validators
 
 from flask_admin.form import Select2Widget
 from flask_admin.contrib.pymongo import ModelView, filters
 from flask_admin.model.fields import InlineFormField, InlineFieldList, FieldList
-import flask_admin.model.fields as f
-print(dir(f))
-print(dir(fields))
+
+
 # Create application
 app = Flask(__name__)
 
@@ -26,12 +24,12 @@ db = conn.Smart_schedule_IRNITU  # Наша база данных
 # TG User admin
 class InnerFormDays(form.Form):
     """списки со временем напоминаний"""
-    понедельник = InlineFieldList(fields.StringField('Время'), 'понедельник')
-    вторник = InlineFieldList(fields.StringField('Время'), 'вторник')
-    среда = InlineFieldList(fields.StringField('Время'), 'среда')
-    четверг = InlineFieldList(fields.StringField('Время'), 'четверг')
-    пятница = InlineFieldList(fields.StringField('Время'), 'пятница')
-    суббота = InlineFieldList(fields.StringField('Время'), 'суббота')
+    понедельник = InlineFieldList(fields.StringField(), 'понедельник')
+    вторник = InlineFieldList(fields.StringField(), 'вторник')
+    среда = InlineFieldList(fields.StringField(), 'среда')
+    четверг = InlineFieldList(fields.StringField(), 'четверг')
+    пятница = InlineFieldList(fields.StringField(), 'пятница')
+    суббота = InlineFieldList(fields.StringField(), 'суббота')
 
 
 class InnerFormWeeks(form.Form):
@@ -41,19 +39,40 @@ class InnerFormWeeks(form.Form):
 
 class UserForm(form.Form):
     """создаём форму"""
-    chat_id = fields.StringField('chat_id')  # поле с chat_id в базе
-    group = fields.StringField('group')  # поле с group в базе
+    chat_id = fields.StringField('chat_id', [validators.DataRequired()])  # поле с chat_id в базе
+    group = fields.SelectField('Группа', widget=Select2Widget())  # поле с group в базе
+    notifications = fields.IntegerField('За сколько минут делать напоминания', default=0)  # поле с chat_id в базе
 
-    reminders = InlineFormField(InnerFormWeeks)  # поле с reminders в базе
+    reminders = InlineFormField(InnerFormWeeks, 'Время напоминаний', default={})  # поле с reminders в базе
 
 
 class UserView(ModelView):
     """создаём отображение формы"""
 
-    column_list = ('chat_id', 'group', 'reminders')  # что будет показываться на странице из формы (какие поля)
-    column_sortable_list = ('chat_id', 'group', 'reminders')  # что сортируется
+    column_list = ('chat_id', 'group', 'notifications')  # что будет показываться на странице из формы (какие поля)
+    column_sortable_list = ('chat_id', 'group', 'notifications')  # что сортируется
+
+    def on_form_prefill(self, form, id):
+        """делает поле chat_id неизменяемым"""
+        form.chat_id.render_kw = {'readonly': True}
 
     form = UserForm
+
+    def _feed_group_choices(self, form):
+        """формируем список групп для выбора"""
+        groups = db.groups.find(fields=('name',))
+        form.group.choices = [group['name'] for group in groups]
+        return form
+
+    def create_form(self):
+        """выводим группы когда создаём"""
+        form = super(UserView, self).create_form()
+        return self._feed_group_choices(form)
+
+    def edit_form(self, obj):
+        """выводим группы когда редактируем"""
+        form = super(UserView, self).edit_form(obj)
+        return self._feed_group_choices(form)
 
 
 # Flask views
