a897123b765f208de1fac51450cf7150371e22b0
bolanebyla <alexleskov2000@gmail.com>
2020-09-28 12:05:25 +0800

    Тестовое отображение коллекции users

--- web_manager/app.py
+++ web_manager/app.py
@@ -18,92 +18,24 @@ app.config['SECRET_KEY'] = '123456790'
 
 # Create models
 conn = pymongo.Connection()
-db = conn.test
+db = conn.Smart_schedule_IRNITU  # Наша база данных
 
 
-# User admin
-class InnerForm(form.Form):
-    name = fields.StringField('Name')
-    test = fields.StringField('Test')
-
-
+# TG User admin
 class UserForm(form.Form):
-    name = fields.StringField('Name')
-    email = fields.StringField('Email')
-    password = fields.StringField('Password')
-
-    # Inner form
-    inner = InlineFormField(InnerForm)
-
-    # Form list
-    form_list = InlineFieldList(InlineFormField(InnerForm))
+    """создаём форму"""
+    chat_id = fields.StringField('chat_id')  # поле с chat_id в базе
+    group = fields.StringField('group')  # поле с group в базе
+    reminders = fields.StringField('reminders')  # поле с reminders в базе
 
 
 class UserView(ModelView):
-    column_list = ('name', 'email', 'password')
-    column_sortable_list = ('name', 'email', 'password')
+    """создаём отображение формы"""
+
+    column_list = ('chat_id', 'group', 'reminders')  # что будет показываться на странице из формы (какие поля)
+    column_sortable_list = ('chat_id', 'group', 'reminders')  # что сортируется
 
     form = UserForm
-
-
-# Tweet view
-class TweetForm(form.Form):
-    name = fields.StringField('Name')
-    user_id = fields.SelectField('User', widget=Select2Widget())
-    text = fields.StringField('Text')
-
-    testie = fields.BooleanField('Test')
-
-
-class TweetView(ModelView):
-    column_list = ('name', 'user_name', 'text')
-    column_sortable_list = ('name', 'text')
-
-    column_filters = (filters.FilterEqual('name', 'Name'),
-                      filters.FilterNotEqual('name', 'Name'),
-                      filters.FilterLike('name', 'Name'),
-                      filters.FilterNotLike('name', 'Name'),
-                      filters.BooleanEqualFilter('testie', 'Testie'))
-
-    column_searchable_list = ('name', 'text')
-
-    form = TweetForm
-
-    def get_list(self, *args, **kwargs):
-        count, data = super(TweetView, self).get_list(*args, **kwargs)
-
-        # Grab user names
-        query = {'_id': {'$in': [x['user_id'] for x in data]}}
-        users = db.user.find(query, fields=('name',))
-
-        # Contribute user names to the models
-        users_map = dict((x['_id'], x['name']) for x in users)
-
-        for item in data:
-            item['user_name'] = users_map.get(item['user_id'])
-
-        return count, data
-
-    # Contribute list of user choices to the forms
-    def _feed_user_choices(self, form):
-        users = db.user.find(fields=('name',))
-        form.user_id.choices = [(str(x['_id']), x['name']) for x in users]
-        return form
-
-    def create_form(self):
-        form = super(TweetView, self).create_form()
-        return self._feed_user_choices(form)
-
-    def edit_form(self, obj):
-        form = super(TweetView, self).edit_form(obj)
-        return self._feed_user_choices(form)
-
-    # Correct user_id reference before saving
-    def on_model_change(self, form, model):
-        user_id = model.get('user_id')
-        model['user_id'] = ObjectId(user_id)
-
-        return model
 
 
 # Flask views
@@ -117,8 +49,7 @@ if __name__ == '__main__':
     admin = admin.Admin(app, name='Example: PyMongo')
 
     # Add views
-    admin.add_view(UserView(db.user, 'Users'))
-    admin.add_view(TweetView(db.tweet, 'Tweets'))
+    admin.add_view(UserView(db.users, 'Users'))
 
     # Start app
     app.run(debug=True)
