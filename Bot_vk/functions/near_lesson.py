b1d80f0604e358e4620e28b1f8ba5b074a8d5cee
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2020-10-21 15:05:33 +0800

    Добавили разделение на текущую и следущую

--- Bot_vk/functions/near_lesson.py
+++ Bot_vk/functions/near_lesson.py
@@ -43,3 +43,40 @@ def get_near_lesson(schedule: list, week: str) -> list:
 
 
     return near_lessons
+
+def get_now_lesson(schedule: list, week: str) -> list:
+    """"Возвращает текущую пару"""
+
+    day_now = datetime.now(TZ_IRKUTSK).strftime('%A')
+    hours_now = int(datetime.now(TZ_IRKUTSK).strftime('%H'))
+    minutes_now = int(datetime.now(TZ_IRKUTSK).strftime('%M'))
+
+    lessons = {}
+    # находим текущий день
+    for one_day in schedule:
+        if one_day['day'].lower() == day_now.lower():
+            lessons = one_day['lessons']
+            break
+    # если сегодняшнего дня нет в расписании значит пар сегодня нет
+    if not lessons:
+        return
+
+    now_lessons = []
+    for lesson in lessons:
+        lesson_week = lesson['week']
+        # смотрим только на пары из текущей недели
+        if lesson_week != week and lesson_week != 'all':
+            continue
+
+        hours_lesson, minutes_lesson = map(int, lesson['time'].split(':'))
+        # находим ближайшую пару
+        a = hours_lesson * 60 + minutes_lesson
+        b = hours_now * 60 + minutes_now
+        if 0 <= b - a <= 90 and not now_lessons:
+            now_lessons.append(lesson)
+        # если несколько пар в одно время (у разных подгрупп)
+        elif now_lessons:
+            if lesson['time'] == now_lessons[0]['time']:
+                now_lessons.append(lesson)
+
+    return now_lessons
