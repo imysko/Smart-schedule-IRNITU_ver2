340100f10275dc1d47649f5ffd8dce41f9d346fa
bolanebyla <alexleskov2000@gmail.com>
2021-01-07 21:47:03 +0800

    Сохранение данных в MongoDB

--- getting_schedule/main.py
+++ getting_schedule/main.py
@@ -1,17 +1,24 @@
 import postgre_storage
 import data_conversion
+from mongo_storage import MongodbService
+
+mongo_storage = MongodbService().get_instance()
 
 
 def main():
+    # Институты
+    # Получаем данные.
     pg_institutes = postgre_storage.get_institutes()
-    print(pg_institutes)
-    mongo_institutes = data_conversion.convert_institutes(pg_institutes)
-    print(mongo_institutes)
+    # Преобразуем данные в нужный формат.
+    mongo_institutes = sorted(data_conversion.convert_institutes(pg_institutes),
+                              key=lambda x: x['name'])
+    # Сохраняем данные.
+    mongo_storage.save_institutes(mongo_institutes)
 
+    # Группы
     pg_groups = postgre_storage.get_groups()
-    print(pg_groups)
     mongo_groups = data_conversion.convert_groups(pg_groups)
-    print(mongo_groups)
+    mongo_storage.save_groups(mongo_groups)
 
 
 if __name__ == '__main__':
