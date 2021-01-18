2c2de5470894135b6ca7c15d466a412f18a0d9f7
bolanebyla <alexleskov2000@gmail.com>
2021-01-18 09:57:38 +0800

    Формирование расписания преподавателей

--- getting_schedule/data_conversion.py
+++ getting_schedule/data_conversion.py
@@ -94,7 +94,7 @@ def convert_schedule(pg_schedule: list) -> list:
 
     date_now = datetime.now(TIME_ZONE).date()
 
-    # date_now = date(2020, 12, 20) ДЛЯ ОТЛАДКИ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # date_now = date(2020, 12, 20)  # ДЛЯ ОТЛАДКИ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     # Сортируем массив, чтобы одинаковые группы стояли рядом.
     pg_schedule = sorted(pg_schedule, key=lambda x: x['obozn'])
@@ -173,6 +173,98 @@ def convert_schedule(pg_schedule: list) -> list:
                 })
 
                 # Обнуляем расписание для слудующей группы
+                schedule = []
+
+        item_index += 1  # Увеличиваем счетчик индекса.
+    return all_schedule
+
+
+def convert_teachers_schedule(pg_schedule: list) -> list:
+    """Преобразование формата расписания преподавателей"""
+
+    date_now = datetime.now(TIME_ZONE).date()
+
+    #date_now = date(2020, 12, 20)  # ДЛЯ ОТЛАДКИ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # Сортируем массив, чтобы одинаковые преподаватели стояли рядом.
+    pg_schedule = sorted(pg_schedule, key=lambda x: x['prep_id'])
+
+    all_schedule = []
+
+    schedule = []  # Расписание преподавателя.
+
+    item_index = 0  # Счетчик индекса.
+    for item in pg_schedule:
+
+        # Проверяем, что расписание действует
+        if date_now <= item['dend']:
+
+            week, day = getting_week_and_day_of_week(item)
+
+            # Определяем вид пары и подгруппу.
+            if item['nt'] == 1:
+                info = '( Лекция )'
+            elif item['nt'] == 2:
+                if item['ngroup']:
+                    info = f'( Практ. подгруппа {item["ngroup"]} )'
+                else:
+                    info = '( Практ. )'
+            else:
+                if item['ngroup']:
+                    info = f'( Лаб. раб. подгруппа {item["ngroup"]} )'
+                else:
+                    info = f'( Лаб. раб. )'
+
+            lesson = {
+                'time': item['begtime'],
+                'week': week,
+                'name': item['title'],
+                'aud': item['auditories_verbose'],
+                'info': info,
+                'groups': [item['obozn']],  ## НУЖНО ДОБАВТЬ НЕСКОЛЬКО ГРУПП
+            }
+
+            # Смотрим, создал ли уже нужный день в расписании.
+            if not is_there_dict_with_value_in_list(schedule, day):
+                schedule.append(
+                    {
+                        'day': day,
+                        'lessons': []
+                    }
+                )
+
+            # Добавляем пары в нужный день.
+            for sch in schedule:
+                if sch['day'] == day:
+                    if lesson in sch['lessons']:
+                        break
+                    sch['lessons'].append(lesson)
+                    break
+
+        # Если нашелся другой преподаватель или это последний элемент списка, сохраняем.
+        current_prep_id = item['prep_id']
+        next_prep_id = ''
+        if item_index != len(pg_schedule) - 1:
+            next_prep_id = pg_schedule[item_index + 1]['prep_id']
+
+        if current_prep_id != next_prep_id or item_index == len(pg_schedule) - 1:
+            # Проверяем, что расписание не пустое
+            if schedule:
+                # Сортируем пары в дне по времени и подгруппе
+                for sch in schedule:
+                    # Сортируем подгруппы
+                    sch['lessons'] = sorted(sch['lessons'], key=lambda x: x['info'])
+                    # Сортируем по времени
+                    sch['lessons'] = sorted(sch['lessons'], key=lambda x: int(x['time'].replace(':', '')))
+
+                all_schedule.append({
+                    'prep': item['preps'].strip(),
+                    'prep_short_name': item['prep_short_name'].strip(),
+                    'pg_id': current_prep_id,
+                    'schedule': sorted(schedule, key=lambda x: get_dict_key(DAYS, x['day']))
+                })
+
+                # Обнуляем расписание для слудующего преподавателя
                 schedule = []
 
         item_index += 1  # Увеличиваем счетчик индекса.
