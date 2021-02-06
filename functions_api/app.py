f361a7d44cfb0a86217f2e5e9a7568765758b3a0
bolanebyla <alexleskov2000@gmail.com>
2021-02-06 12:43:01 +0800

    Формирование расписания

---
+++ functions_api/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, make_response, jsonify
+
+from functions import find_week, creating_schedule
+
+app = Flask(__name__)
+
+
+@app.route('/api/find_week/')
+def find_week_route():
+    result = find_week.find_week()
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/full_schedule_in_str/')
+def full_schedule_in_str_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.full_schedule_in_str(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/get_one_day_schedule_in_str/')
+def get_one_day_schedule_in_str_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.get_one_day_schedule_in_str(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/get_next_day_schedule_in_str/')
+def get_next_day_schedule_in_str_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.get_next_day_schedule_in_str(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/get_one_day_schedule_in_str_prep/')
+def get_one_day_schedule_in_str_prep_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.get_one_day_schedule_in_str_prep(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/get_next_day_schedule_in_str_prep/')
+def get_next_day_schedule_in_str_prep_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.get_next_day_schedule_in_str_prep(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
+
+
+@app.route('/api/creating_schedule/full_schedule_in_str_prep/')
+def full_schedule_in_str_prep_route():
+    data = request.json
+    if not data:
+        return make_response("Bad request", 400)
+    try:
+        result = creating_schedule.full_schedule_in_str_prep(**data)
+    except TypeError:
+        return make_response("Bad request", 400)
+    return jsonify(result)
--- functions_api/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from app import views
-
-app = Flask(__name__)
-
-app.add_url_rule('/api/find_week/', view_func=views.FindWeekView.as_view('find_week'))
