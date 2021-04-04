32d79e61ac597547067cf8d55c494a581a33dbcc
JlblCblU4EPT <49277810+JlblCblU4EPT@users.noreply.github.com>
2021-04-04 20:51:56 +0800

    Пофиксить консольный запуск

--- getting_schedule/functions/get_exams.py
+++
@@ -1,9 +0,0 @@
-from tools.storage import MongodbService
-
-storage = MongodbService().get_instance()
-
-
-def groups_exam(group):
-    schedule = storage.get_schedule_exam(group=group)
-
-    return schedule
