38f387761cd67552e6f2811cb93fed8bba65d635
Алексей <alexleskov2000@gmail.com>
2021-04-14 08:57:13 +0800

    Запуск функции каждые 60 секунд

--- notification_service/reminder.py
+++ notification_service/reminder.py
@@ -1,6 +1,7 @@
 import locale
 import os
 import platform
+import threading
 from datetime import datetime
 from typing import Literal
 
@@ -78,47 +79,46 @@ class Reminder:
             raise ValueError(f'Нет такой платформы: {self.platform}')
 
     def search_for_reminders(self):
+        logger.info(f'{self.platform} сработало')
+        # определяем время сейчас
+        time_now = datetime.now(TZ_IRKUTSK)
+        day_now = datetime.now(TZ_IRKUTSK).strftime('%A').lower()
 
-        minutes_old = None
-        while True:
-            # определяем время сейчас
-            time_now = datetime.now(TZ_IRKUTSK)
-            day_now = datetime.now(TZ_IRKUTSK).strftime('%A').lower()
+        # выполняем функцию каждые 60 секунд
+        threading.Timer(60, self.search_for_reminders).start()
 
-            minutes_now = time_now.strftime('%M')
+        # minutes_now = time_now.strftime('%M')
 
-            if minutes_old != minutes_now:
-                minutes_old = minutes_now  # нужно для того чтобы выполнить тело только один раз
-                self.users = []
+        self.users = []
 
-                # получаем пользователей у которых включены напоминания
-                if self.platform == 'vk':
-                    reminders = self.storage.get_users_with_reminders_vk()
-                elif self.platform == 'tg':
-                    reminders = self.storage.get_users_with_reminders_tg()
+        # получаем пользователей у которых включены напоминания
+        if self.platform == 'vk':
+            reminders = self.storage.get_users_with_reminders_vk()
+        elif self.platform == 'tg':
+            reminders = self.storage.get_users_with_reminders_tg()
 
-                for reminder in reminders:
-                    week = tools.find_week()
+        for reminder in reminders:
+            week = tools.find_week()
 
-                    if 'reminders' not in reminder.keys():
-                        continue
+            if 'reminders' not in reminder.keys():
+                continue
 
-                    # если у пользователя пустой reminders то None
-                    user_days = reminder['reminders'].get(week)
-                    if not user_days:
-                        continue
-                    # если у пользователя нет ткущего дня, то None
-                    user_day_reminder_time = user_days.get(day_now.lower())
+            # если у пользователя пустой reminders то None
+            user_days = reminder['reminders'].get(week)
+            if not user_days:
+                continue
+            # если у пользователя нет ткущего дня, то None
+            user_day_reminder_time = user_days.get(day_now.lower())
 
-                    # если время совпадает с текущим, добавляем в список на отправку
-                    if tools.check_that_user_has_reminder_enabled_for_the_current_time(time_now,
-                                                                                       user_day_reminder_time):
-                        chat_id = reminder['chat_id']
-                        group = reminder['group']
-                        notifications = reminder['notifications']
+            # если время совпадает с текущим, добавляем в список на отправку
+            if tools.check_that_user_has_reminder_enabled_for_the_current_time(time_now,
+                                                                               user_day_reminder_time):
+                chat_id = reminder['chat_id']
+                group = reminder['group']
+                notifications = reminder['notifications']
 
-                        user = tools.forming_user_to_submit(chat_id, group, notifications, day_now, time_now, week)
-                        self.users.append(user)
+                user = tools.forming_user_to_submit(chat_id, group, notifications, day_now, time_now, week)
+                self.users.append(user)
 
-                # после того как список сформирован, нужно отправить его боту
-                self.sending_notifications()
+        # после того как список сформирован, нужно отправить его боту
+        self.sending_notifications()
