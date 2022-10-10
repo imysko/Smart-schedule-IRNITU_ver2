76c14fc4d68429971827eacd6880b1264c3941b9
imysko <mysko2002@mail.ru>
2022-10-10 21:09:40 +0800

    Deleted All

--- tools/schedule_tools/get_text_schedule_not_available.py
+++
@@ -1,3 +0,0 @@
-def get_text_schedule_not_available() -> str:
-    text = 'Расписание временно недоступно🚫😣\nПопробуйте позже⏱'
-    return text
--- tools/vk_tools/state.py
+++
@@ -1,7 +0,0 @@
-from vkbottle_types import BaseStateGroup
-
-
-class SuperStates(BaseStateGroup):
-    SEARCH = 0
-    PREP_REG = 1
-    AUD_SEARCH = 2
