545687c9a5498aefba493d64eb3d47503ac9f830
m <mmailm@mail.ru>
2023-01-16 13:22:00 +0800

    fix

--- generate_sqlite_backup_zip.py
+++ generate_sqlite_backup_zip.py
@@ -17,7 +17,7 @@ try:
     with zipfile.ZipFile(filename, 'w', compresslevel=9, compression=zipfile.ZIP_BZIP2) as zf:
         zf.write(fp.name, "test.db")
 
-    os.unlink("sqlite_db.zip")
-    os.symlink(filename, "sqlite_db.zip")
+    os.unlink("sqlite.db.zip")
+    os.symlink(filename, "sqlite.db.zip")
 finally:
     os.unlink(fp.name)
